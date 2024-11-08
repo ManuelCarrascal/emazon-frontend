16dc1a28c8a52857ec10855cf69ea8cd59443f28
Manuel Carrascal <tricdiart@gmail.com>
2024-11-08 15:16:54 -0500

    feat: Base Logic for view products in cart interface

--- src/app/modules/cart/cart.module.ts
+++ src/app/modules/cart/cart.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CartComponent } from './cart.component';
+import { SharedModule } from '@/app/shared/shared.module';
+import { LucideAngularModule} from 'lucide-angular';
 
 
 
 @NgModule({
   declarations: [CartComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
+    LucideAngularModule
   ]
 })
 export class CartModule { }
