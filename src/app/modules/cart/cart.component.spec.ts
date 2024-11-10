dc0f98f385aeba4d902cce3619c17008aac08d2a
Manuel Carrascal <tricdiart@gmail.com>
2024-11-09 21:48:29 -0500

    test: Implementation of unit tests for cart component and cart service

--- src/app/modules/cart/cart.component.spec.ts
+++ src/app/modules/cart/cart.component.spec.ts
@@ -1,23 +1,136 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { of, throwError } from 'rxjs';
 import { CartComponent } from './cart.component';
+import { CartService } from '@/app/shared/services/cart/cart.service';
+import { CartResponse, CartProduct } from '@/app/shared/interfaces/cart.interface';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('CartComponent', () => {
   let component: CartComponent;
   let fixture: ComponentFixture<CartComponent>;
+  let cartService: CartService;
+
+  const mockCartProducts: CartProduct[] = [
+    {
+      productId: 1,
+      productName: 'Product 1',
+      productDescription: 'Description 1',
+      productQuantity: 10,
+      productPrice: 100,
+      cartQuantity: 2,
+      brand: {
+        brandName: 'Brand 1',
+        brandDescription: 'Description of Brand 1',
+      },
+      categories: [
+        {
+          categoryName: 'Category 1',
+          categoryDescription: 'Description of Category 1',
+        },
+      ],
+      subtotal: 200,
+    },
+    {
+      productId: 2,
+      productName: 'Product 2',
+      productDescription: 'Description 2',
+      productQuantity: 20,
+      productPrice: 200,
+      cartQuantity: 1,
+      brand: {
+        brandName: 'Brand 2',
+        brandDescription: 'Description of Brand 2',
+      },
+      categories: [
+        {
+          categoryName: 'Category 2',
+          categoryDescription: 'Description of Category 2',
+        },
+      ],
+      subtotal: 200,
+    },
+  ];
+
+  const mockCartResponse: CartResponse = {
+    content: mockCartProducts,
+    totalElements: 2,
+    totalPages: 1,
+    currentPage: 0,
+    ascending: true,
+    empty: false,
+    total: 400,
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CartComponent ]
-    })
-    .compileComponents();
+      declarations: [CartComponent],
+      imports: [HttpClientTestingModule],
+      providers: [
+        {
+          provide: CartService,
+          useValue: {
+            getCart: jest.fn().mockReturnValue(of(mockCartResponse)),
+            removeProductFromCart: jest.fn().mockReturnValue(of({})),
+          },
+        },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(CartComponent);
     component = fixture.componentInstance;
+    cartService = TestBed.inject(CartService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should load cart on init', () => {
+    jest.spyOn(cartService, 'getCart').mockReturnValue(of(mockCartResponse));
+    component.ngOnInit();
+    expect(cartService.getCart).toHaveBeenCalledWith(component.size, true);
+    expect(component.cartProducts).toEqual(mockCartResponse.content);
+    expect(component.filteredProducts).toEqual(mockCartResponse.content);
+    expect(component.total).toBe(mockCartResponse.total);
+  });
+
+  it('should update cart', () => {
+    component.cartProducts = mockCartProducts;
+    component.searchTerm = 'Product 1';
+    component.updateCart();
+    expect(component.filteredProducts.length).toBe(1);
+    expect(component.filteredProducts[0].productName).toBe('Product 1');
+  });
+
+  it('should remove product from cart', () => {
+    jest.fn().mockReturnValue(of(''));
+    component.cartProducts = mockCartProducts;
+    component.filteredProducts = [...mockCartProducts];
+    component.removeFromCart(1);
+    expect(cartService.removeProductFromCart).toHaveBeenCalledWith(1);
+    expect(component.cartProducts.length).toBe(1);
+    expect(component.filteredProducts.length).toBe(1);
+    expect(component.total).toBe(200);
+  });
+
+  it('should handle error when loading cart', () => {
+    jest.spyOn(cartService, 'getCart').mockReturnValue(throwError(() => new Error('Error')));
+    component.loadCart();
+    expect(component.cartProducts).toEqual([]);
+    expect(component.filteredProducts).toEqual([]);
+    expect(component.total).toBe(0);
+  });
+
+  it('should handle error when removing product from cart', () => {
+    jest
+      .spyOn(cartService, 'removeProductFromCart')
+      .mockReturnValue(throwError(() => new Error('Error')));
+    component.cartProducts = mockCartProducts;
+    component.filteredProducts = [...mockCartProducts];
+    component.removeFromCart(1);
+    expect(component.cartProducts.length).toBe(2);
+    expect(component.filteredProducts.length).toBe(2);
+    expect(component.total).toBe(400);
   });
 });
--- src/app/modules/cart/cart.component.ts
+++ src/app/modules/cart/cart.component.ts
@@ -1,11 +1,14 @@
-import { CartResponse, CartProduct } from '@/app/shared/interfaces/cart.interface';
+import {
+  CartResponse,
+  CartProduct,
+} from '@/app/shared/interfaces/cart.interface';
 import { CartService } from '@/app/shared/services/cart/cart.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.scss']
+  styleUrls: ['./cart.component.scss'],
 })
 export class CartComponent implements OnInit {
   cartProducts: CartProduct[] = [];
@@ -14,7 +17,7 @@ export class CartComponent implements OnInit {
   searchTerm: string = '';
   size: number = 5;
 
-  constructor(private readonly cartService: CartService) { }
+  constructor(private readonly cartService: CartService) {}
 
   ngOnInit(): void {
     this.loadCart();
@@ -27,24 +30,36 @@ export class CartComponent implements OnInit {
       next: (data: CartResponse) => {
         this.cartProducts = data.content;
         this.filteredProducts = [...this.cartProducts];
-        this.total = data.total; 
+        this.total = data.total;
         console.log('Cart data:', data);
       },
       error: (error) => {
         console.error('Error fetching cart data:', error);
-      }
+        this.cartProducts = [];
+        this.filteredProducts = [];
+        this.total = 0;
+      },
     });
   }
 
   updateCart(): void {
     const term = this.searchTerm.toLowerCase();
-    this.filteredProducts = this.cartProducts.filter(product => {
-      const matchesCategory = product.categories?.some(category => category.categoryName.toLowerCase().includes(term));
-      const matchesBrand = product.brand?.brandName.toLowerCase().includes(term);
-      const matchesProductName = product.productName.toLowerCase().includes(term);
+    this.filteredProducts = this.cartProducts.filter((product) => {
+      const matchesCategory = product.categories?.some((category) =>
+        category.categoryName.toLowerCase().includes(term)
+      );
+      const matchesBrand = product.brand?.brandName
+        .toLowerCase()
+        .includes(term);
+      const matchesProductName = product.productName
+        .toLowerCase()
+        .includes(term);
       return matchesCategory || matchesBrand || matchesProductName;
     });
-    this.total = this.filteredProducts.reduce((acc, product) => acc + product.subtotal, 0);
+    this.total = this.filteredProducts.reduce(
+      (acc, product) => acc + product.subtotal,
+      0
+    );
   }
 
   removeFromCart(productId: number): void {
@@ -52,13 +67,20 @@ export class CartComponent implements OnInit {
     this.cartService.removeProductFromCart(productId).subscribe({
       next: () => {
         console.log('Product removed from cart');
-        this.cartProducts = this.cartProducts.filter(product => product.productId !== productId);
-        this.filteredProducts = this.filteredProducts.filter(product => product.productId !== productId);
-        this.total = this.filteredProducts.reduce((acc, product) => acc + product.subtotal, 0); 
+        this.cartProducts = this.cartProducts.filter(
+          (product) => product.productId !== productId
+        );
+        this.filteredProducts = this.filteredProducts.filter(
+          (product) => product.productId !== productId
+        );
+        this.total = this.filteredProducts.reduce(
+          (acc, product) => acc + product.subtotal,
+          0
+        );
       },
       error: (error) => {
         console.error('Error removing product from cart:', error);
-      }
+      },
     });
   }
 }
